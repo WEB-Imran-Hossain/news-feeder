721cdad81a4308d88126eb6237c2025419eeb24f
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-07-14 21:02:04 +0600

    create newsboard and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Logo from "./components/header/Logo";
+import NewsBoard from "./components/news/NewsBoard";
 
 function App() {
-  return <div>
-    <Logo />
-    </div>;
+  return (
+    <div>
+      <Logo />
+      <NewsBoard />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
